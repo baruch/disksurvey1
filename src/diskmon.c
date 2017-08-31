4e1f0bb6db90192afbe94e22fe565c3676bf77b4
Baruch Even <baruch@weka.io>
2017-08-31 16:28:08 +0300

    Send an inquiry command to know the disk identity

--- src/diskmon.c
+++ src/diskmon.c
@@ -1,19 +1,23 @@
 #include "diskmon.h"
 #include "common.h"
 
+#include <scsicmd.h>
+
 #include <scsi/sg.h>
 #include <errno.h>
 #include <memory.h>
 #include <assert.h>
 #include <stdbool.h>
 
-static bool disk_mon_send_cmd(disk_entry_t* disk, sg_io_hdr_t* io, int dir, unsigned char* cmd, int cmd_len, unsigned char* sense, int sense_len)
+static bool disk_mon_send_cmd(disk_entry_t* disk, sg_io_hdr_t* io, int dir, unsigned char* cmd, int cmd_len, unsigned char* sense, int sense_len, unsigned char* buf, int buf_sz)
 {
     memset(io, 0, sizeof(*io));
     io->interface_id = 'S';
     io->dxfer_direction = dir;
     io->cmd_len = cmd_len;
     io->mx_sb_len = sense_len;
+    io->dxfer_len = buf_sz;
+    io->dxferp = buf;
     io->cmdp = cmd;
     io->sbp = sense;
     io->timeout = 30*1000;
@@ -55,20 +59,20 @@ static bool disk_mon_recv_reply(disk_entry_t* disk, sg_io_hdr_t* io, wire_fd_sta
     return true;
 }
 
-bool disk_mon_cmd(disk_entry_t* disk, wire_fd_state_t* fd_state, int dir, unsigned char* cmd, int cmd_len, unsigned char* sense, int sense_len)
+int disk_mon_cmd(disk_entry_t* disk, wire_fd_state_t* fd_state, int dir, unsigned char* cmd, int cmd_len, unsigned char* sense, int sense_len, unsigned char* buf, int buf_sz)
 {
     sg_io_hdr_t io;
 
-    if (!disk_mon_send_cmd(disk, &io, SG_DXFER_NONE, cmd, sizeof(cmd), sense, sizeof(sense)))
-        return false;
+    if (!disk_mon_send_cmd(disk, &io, dir, cmd, sizeof(cmd), sense, sizeof(sense), buf, buf_sz))
+        return -1;
 
     if (!disk_mon_recv_reply(disk, &io, fd_state))
-        return false;
+        return -1;
 
     // TODO: process reply, be more discernible regarding status values
     if (io.status != 0) {
         wire_log(WLOG_INFO, "Device %s fd %d status %08x, breaking", disk->dev_name, disk->fd, io.status);
-        return false;
+        return -1;
     }
 
     if (io.sb_len_wr != 0) {
@@ -77,30 +81,65 @@ bool disk_mon_cmd(disk_entry_t* disk, wire_fd_state_t* fd_state, int dir, unsign
         return false;
     }
 
-    return true;
+    return buf_sz - io.resid;
 }
 
 void disk_mon_wire(void* _disk)
 {
     disk_entry_t* disk = _disk;
     wire_fd_state_t fd_state;
+    unsigned char cmd[16];
+    unsigned char sense[128];
+    int buf_len;
+    unsigned char buf[1024];
+    int cmd_len;
+    int device_type;
+    scsi_vendor_t vendor;
+    scsi_model_t model;
+    scsi_fw_revision_t rev;
+    scsi_serial_t serial;
 
     wire_log(WLOG_INFO, "Starting monitor of disk %s fd %d ptr %p", disk->dev_name, disk->fd, disk);
     wire_fd_mode_init(&fd_state, disk->fd);
 
-    while (1) {
-        unsigned char sense[128];
-        unsigned char cmd[6];
+    cmd_len = cdb_inquiry_simple(cmd, 96);
+    buf_len = disk_mon_cmd(disk, &fd_state, SG_DXFER_FROM_DEV, cmd, cmd_len, sense, sizeof(sense), buf, 96);
+    if (buf_len < 0) {
+        wire_log(WLOG_ERR, "Error on inquiry command");
+        goto Exit;
+    }
 
+    wire_log(WLOG_DEBUG, "inquiry len %d", buf_len);
+    {
+        int idx;
+        int line_idx;
+
+        for (idx = 0; idx < buf_len; idx++, line_idx++) {
+            if (line_idx == 16) {
+                printf("\n");
+                line_idx = 0;
+            }
+
+            printf("%02x ", buf[idx]);
+        }
+    }
+    if (!parse_inquiry(buf, buf_len, &device_type, vendor, model, rev, serial)) {
+        wire_log(WLOG_INFO, "Inquiry data parsing failed");
+        return;
+    }
+    wire_log(WLOG_INFO, "Disk %s type %d vendor %s model %s rev %s serial %s", disk->dev_name, device_type, vendor, model, rev, serial);
+
+    while (1) {
         wire_fd_wait_msec(1000);
         wire_log(WLOG_DEBUG, "ping %s", disk->dev_name);
 
-        memset(cmd, 0, sizeof(cmd)); // TUR, all zeroes
-        if (!disk_mon_cmd(disk, &fd_state, SG_DXFER_NONE, cmd, sizeof(cmd), sense, sizeof(sense)))
+        memset(cmd, 0, 6); // TUR, all zeroes
+        if (disk_mon_cmd(disk, &fd_state, SG_DXFER_NONE, cmd, 6, sense, sizeof(sense), NULL, 0) < 0)
             break;
     }
 
     // close the disk fd and mark the entry as dead (fd == -1)
+Exit:
     wire_log(WLOG_INFO, "Finished monitor of disk %s fd %d", disk->dev_name, disk->fd);
     close(disk->fd);
     disk->fd = -1;
